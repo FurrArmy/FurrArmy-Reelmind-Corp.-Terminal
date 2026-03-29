5667dbd827c9a3445af6cdd15ca628b679e63407
FurrArmy <FurrArmy@web.de>
2026-03-29 19:37:26 +0200

    Create docs.lua

---
+++ FurrArmy/docs.lua
@@ -0,0 +1,12 @@
+return {
+    {
+        title = "Operation Night Fang",
+        clearance = 2,
+        content = "Recon units deployed to sector 7..."
+    },
+    {
+        title = "Omega Protocol",
+        clearance = 5,
+        content = "In case of total collapse, initiate Code Black..."
+    }
+}
