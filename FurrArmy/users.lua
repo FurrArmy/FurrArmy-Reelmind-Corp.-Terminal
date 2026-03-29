047eea1ec01e2d80437e0dfaab80ca089b791111
FurrArmy <FurrArmy@web.de>
2026-03-29 19:35:04 +0200

    Create users.lua

---
+++ FurrArmy/users.lua
@@ -0,0 +1,16 @@
+return {
+    {
+        username = "Mike",
+        password = "1505",
+        division = "Federal Government",
+        rank = "FGC-1",
+        clearance = 10
+    },
+    {
+        username = "scout1",
+        password = "pass",
+        division = "Recon",
+        rank = "Scout",
+        clearance = 2
+    }
+}
